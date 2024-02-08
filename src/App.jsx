b11a7a9b9f8b1761b5bc5bfa201d6a8063b084f7
Andrey Zinoviev <dronis_dronis@mail.ru>
2024-02-08 17:35:12 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+//functions
+function App() {
+  return (
+    <section>
+      <h1>Проект для Никиты</h1>
+      <p>Нужно на кнопки добавить функции, чтобы кнопки работали</p>
+      <div>
+        <button>Поменять фон с белого на черный</button>
+        <button>Показать спрятанную картинку</button>
+        <button>
+          <p>Включать\выключать другой цвет текста</p>
+        </button>
+      </div>
+    </section>
+  )
+}
+
+export default App
